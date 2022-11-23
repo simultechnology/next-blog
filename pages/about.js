94bdf6d1aa50d06d1e0f9e00dbe3b7cb0854b37e
t_ishikawa <tissi0708@gmail.com>
2022-11-24 03:14:43 +0900

    add contact.js and two-column.js

--- pages/about.js
+++ pages/about.js
@@ -1,27 +1,48 @@
 import Container from "@/components/container";
 import {Hero} from "@/components/hero";
 import PostBody from "@/components/post-body";
+import Contact from "@/components/contact";
+import {TwoColumn, TwoColumnMain, TwoColumnSidebar} from "@/components/two-column";
 
 export default function About() {
   return (
     <Container>
       <Hero title="About" subtitle="About development activities" />
-      <PostBody>
-        <p>
-          test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-
-        </p>
-        <h2>h2 title aaaaaaa</h2>
-        <p>
-          test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-
-        </p>
-        <p>
-          test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-
-        </p>
-        <h3>h3 title aaaaaaaa</h3>
-        <p>
-          test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-
-        </p>
-      </PostBody>
+      <TwoColumn>
+        <TwoColumnMain>
+          <PostBody>
+            <p>
+              test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd- <br />
+              test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd- <br />
+              test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd- <br />
+              test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-
+            </p>
+            <h2>h2 title aaaaaaa</h2>
+            <p>
+              test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd- <br />
+              test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd- <br />
+              test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd- <br />
+              test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-
+            </p>
+            <p>
+              test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd- <br />
+              test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd- <br />
+              test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd- <br />
+              test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-
+            </p>
+            <h3>h3 title aaaaaaaa</h3>
+            <p>
+              test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd- <br />
+              test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd- <br />
+              test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd- <br />
+              test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-
+            </p>
+          </PostBody>
+        </TwoColumnMain>
+        <TwoColumnSidebar>
+          <Contact />
+        </TwoColumnSidebar>
+      </TwoColumn>
     </Container>
   )
 }
