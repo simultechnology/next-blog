0b70759f91acc2e94615e2fcfef1483ea790feca
t_ishikawa <tissi0708@gmail.com>
2022-11-24 01:06:39 +0900

    add post-body.js

--- pages/about.js
+++ pages/about.js
@@ -1,25 +1,27 @@
 import Container from "@/components/container";
 import {Hero} from "@/components/hero";
+import PostBody from "@/components/post-body";
 
 export default function About() {
   return (
     <Container>
       <Hero title="About" subtitle="About development activities" />
-
-      <p>
-        test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-
-      </p>
-      <h2>h2 title aaaaaaa</h2>
-      <p>
-        test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-
-      </p>
-      <p>
-        test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-
-      </p>
-      <h3>h3 title aaaaaaaa</h3>
-      <p>
-        test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-
-      </p>
+      <PostBody>
+        <p>
+          test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-
+        </p>
+        <h2>h2 title aaaaaaa</h2>
+        <p>
+          test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-
+        </p>
+        <p>
+          test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-
+        </p>
+        <h3>h3 title aaaaaaaa</h3>
+        <p>
+          test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-test-abcd-
+        </p>
+      </PostBody>
     </Container>
   )
 }
