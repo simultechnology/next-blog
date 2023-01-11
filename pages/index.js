a561c7283a2e5ab025993d17472c8b9ab88ea762
t_ishikawa <tissi0708@gmail.com>
2023-01-11 10:09:24 +0900

    show article list in index page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import Container from "@/components/container";
 import React from 'react'
-import Posts from "./posts";
+import Posts from "@/components/posts";
 import Hero from "@/components/hero";
 import Meta from "@/components/meta";
+import {getAllPosts} from "../lib/api";
+import {eyecatchLocal} from "../lib/constants";
+import {getPlaiceholder} from "plaiceholder";
+import Pagination from "@/components/pagination";
 
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <Container>
       <Meta />
@@ -13,8 +17,27 @@ export default function Home() {
         subtitle="site for output"
         imageOn
       />
-      <Posts />
+      <Posts posts={posts}/>
+      <Pagination nextUrl={"/blog"} nextText={"More Posts"} />
     </Container>
   )
 }
 
+export async function getStaticProps() {
+  const posts = await getAllPosts(4)
+
+  for (const post of posts) {
+    if (!post.hasOwnProperty('eyecatch')) {
+      post.eyecatch = eyecatchLocal
+    }
+    console.log(post)
+    const { base64 } = await getPlaiceholder(post.eyecatch.url);
+    post.eyecatch.blurDataURL = base64
+  }
+
+  return {
+    props: {
+      posts: posts,
+    },
+  }
+}
